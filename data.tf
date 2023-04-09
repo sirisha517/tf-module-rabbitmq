cc9e839bc0aca9d8925847dfdcb3866ed4f62a97
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-09 12:27:15 +0530

    rabbitmq

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-prac-with-ansible	" //AMI NAME
+  name_regex       = "devops-prac-with-ansible" //AMI NAME
   owners           = ["self"] //OWNER
 }
