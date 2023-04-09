c80b4ec04c5a4913dc30e1d5c6e70ad9f813e185
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-09 12:21:52 +0530

    rabbitmq

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resource "aws_spot_instance_request" "cheap_worker" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  subnet_id = var.subnet_ids[0]
+resource "aws_spot_instance_request" "rabbitmq" {
+  ami                  = data.aws_ami.ami.id
+  instance_type        = var.instance_type
+  subnet_id            = var.subnet_ids[0]
   wait_for_fulfillment = true
+
 
   tags = merge(
     var.tags,
--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
 variable "env" {}
 variable "tags" {}
-variable "instance_type" {}
 variable "subnet_ids" {}
+variable "instance_type" {}
