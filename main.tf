01f7f10b267a6f97a24a15e6d6d6c588e9a05c6d
sirisha517 <sai.sirifriends8790@gmail.com>
2023-05-03 22:43:17 +0530

    fix shipping

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_spot_instance_request" "rabbitmq" {
-  ami                  = data.aws_ami.ami.id
-  instance_type        = var.instance_type
-  subnet_id            = var.subnet_ids[0]
-  wait_for_fulfillment = true
-  vpc_security_group_ids = aws_security_group.main.id
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_ids[0]
+  wait_for_fulfillment   = true
+  vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile   = aws_iam_instance_profile.main.name
 
   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     component = "rabbitmq"
@@ -15,6 +16,14 @@ resource "aws_spot_instance_request" "rabbitmq" {
     { Name = "${var.env}-rabbitmq" }
   )
 }
+
+resource "aws_ec2_tag" "name-tag" {
+  key         = "Name"
+  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
+  value       = "rabbitmq-${var.env}"
+}
+
+
 
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.domain.zone_id
@@ -37,6 +46,7 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.allow_subnets
   }
+
   ingress {
     description = "SSH"
     from_port   = 22
@@ -59,8 +69,4 @@ resource "aws_security_group" "main" {
   )
 }
 
-resource "aws_ec2_tag" "name_tag" {
-  key         = "Name"
-  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
-  value       = "rabbitmq-${var.env}"
-}
+
